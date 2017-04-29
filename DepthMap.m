10c492984b95f3ce73cfee981b6a3d362945f7a1
DESKTOP-SCVHRLK\Denis <andreev.93@gmail.com>
2017-04-30 02:25:10 +0300

    interface

--- DepthMap.m
+++ DepthMap.m
@@ -2,78 +2,40 @@ close all;
 clear all;
 clc;
 
-resize=1;
-
+%%
+%входные данные и параметры
+left=rgb2gray(imread('Teddy_L.png'));
+right=rgb2gray(imread('Teddy_R.png'));
+[rows,cols]=size(left);
 min_disparity=0;
 max_disparity=16*4;
 P1=8;
 P2=32;
 
-%
-%загрузка изображений
-
-left=rgb2gray(imresize(imread('Teddy_L.png'),resize));
-right=rgb2gray(imresize(imread('Teddy_R.png'),resize));
-[rows,cols]=size(left);
-
-
-
+%%
+%вывод изображений на экран
 figure;
 imshow(left,[0 255]);
 figure;
 imshow(right,[0 255]);
 
-resize_0=1/8;
-left_0=imresize(left,resize_0);
-right_0=imresize(right,resize_0);
-max_disparity_0=max_disparity*resize_0;
-init_disp=zeros(size(left_0),'uint8');
-
-disparity_est=stereo_sg_func( left_0, right_0 , init_disp, max_disparity_0, P1, P2);
-
-figure;
-imshow(disparity_est,[min_disparity max_disparity]);
-
-resize_1=1/4;
-left_1=imresize(left,resize_1);
-right_1=imresize(right,resize_1);
-max_disparity_1=max_disparity*resize_1;
-disparity_est=round(imresize(disparity_est,size(left_1)));
-disparity_est=stereo_sg_func( left_1, right_1 , disparity_est, max_disparity_1, P1, P2);
-
-figure;
-imshow(disparity_est,[min_disparity max_disparity]);
-
-resize_2=1/2;
-left_2=imresize(left,resize_2);
-right_2=imresize(right,resize_2);
-max_disparity_2=max_disparity*resize_2;
-disparity_est=round(imresize(disparity_est,size(left_2)));
-disparity_est=stereo_sg_func( left_2, right_2 , disparity_est, max_disparity_2, P1, P2);
-
-figure;
-imshow(disparity_est,[min_disparity max_disparity]);
-
-resize_3=1;
-left_3=imresize(left,resize_3);
-right_3=imresize(right,resize_3);
-max_disparity_3=max_disparity*resize_3;
-disparity_est=round(imresize(disparity_est,size(left_3)));
-disparity_est=stereo_sg_func( left_3, right_3 , disparity_est, max_disparity_3, P1, P2);
-
-figure;
-imshow(disparity_est,[min_disparity max_disparity]);
-
-
-
 %%
-%[M,N]=size(im1{1}); %размер изображения
-B=0.2; %база (расстояние между центрами камер)
-f=0.025; %фокусное расстояние
-l=6.5*10^(-6); %размер пиксела
+%расчет карты глубины
+disparity_map=stereo_sg_func( left, right , max_disparity, P1, P2);
+%вывод карты глубины на экран
+figure;
+imshow(disparity_map,[min_disparity max(max(disparity_map))]);
 
-%размер блока
-blk_sz=7;
+
+
+% %%
+% %[M,N]=size(im1{1}); %размер изображения
+% B=0.2; %база (расстояние между центрами камер)
+% f=0.025; %фокусное расстояние
+% l=6.5*10^(-6); %размер пиксела
+% 
+% %размер блока
+% blk_sz=7;
 
 
 %%
