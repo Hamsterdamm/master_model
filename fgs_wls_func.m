172c44422205ad2bff157bc8a1a52a8e198ecc4e
DESKTOP-SCVHRLK\Denis <andreev.93@gmail.com>
2017-04-30 18:21:22 +0300

    interpolation

--- fgs_wls_func.m
+++ fgs_wls_func.m
@@ -1,4 +1,4 @@
-function [ output ] = fgs_wls_func( input, guidance, lambda, T, varargin )
+function [ output ] = fgs_wls_func( input, guidance, lambda, T, interpolate )
 %UNTITLED7 Summary of this function goes here
 %   Detailed explanation goes here
 
@@ -11,8 +11,8 @@ for t=1:T
     waitbar(t/T)
     
     lam=3/2*((4^(T-t))/(4^T-1))*lambda;
-    temp=fgs_wls_h(temp,guidance,lam);
-    temp=fgs_wls_v(temp,guidance,lam);
+    temp=fgs_wls_h(temp,guidance,lam, interpolate);
+    temp=fgs_wls_v(temp,guidance,lam, interpolate);
 end
 
 output=temp;
@@ -21,70 +21,7 @@ close(h);
 
 end
 
-function [ output ] = fgs_wls_v( input, guidance, lambda, varargin )
-%UNTITLED6 Summary of this function goes here
-%   Detailed explanation goes here
-
-output=zeros(size(input));
-out_ind=zeros(size(input));
-[rows,cols]=size(input);
-weights=zeros(rows,rows);
-laplacian=zeros(rows,rows);
-I=eye(rows);
-
-for x=1:cols
-
-f=input(:,x);
-g=guidance(:,x);
-variance=var(g);
-
-for p=1:rows
-    
-    for q=1:rows
-        
-        weights(p,q)=exp(-abs(g(p)-g(q))/variance);
-        
-    end
-    
-end
-
-for m=1:rows
-    
-    for n=1:rows
-        
-            
-        if (m==n)
-            if (m==1)
-                laplacian(m,n)=weights(m,m+1);
-            elseif (m==rows)
-                laplacian(m,n)=weights(m,m-1);
-            else
-                laplacian(m,n)=weights(m,m-1)+weights(m,m+1);
-            end
-        elseif (n==(m-1))||(n==(m+1))
-            laplacian(m,n)=-weights(m,n);
-        else
-            laplacian(m,n)=0;
-        end
-        
-
-    end
-    
-end
-
-output(:,x)=(I+lambda*laplacian)\f;
-
-if size(varargin)>0
-index=f(f>5);
-out_ind(:,x)=(I+lambda*laplacian)\index;
-output(:,x)=output(:,x)./out_ind(:,x);
-end
-
-end
-
-end
-
-function [ output ] = fgs_wls_h( input, guidance, lambda, varargin )
+function [ output ] = fgs_wls_h( input, guidance, lambda, interpolate )
 %UNTITLED6 Summary of this function goes here
 %   Detailed explanation goes here
 
@@ -137,13 +74,79 @@ end
 
 output(y,:)=(I+lambda*laplacian)\transpose(f);
 
-if size(varargin)>0
-index=f(f>5);
-out_ind(y,:)=(I+lambda*laplacian)\transpose(index);
-output(y,:)=output(y,:)./out_ind(y,:);
+if (interpolate==1)
+    index=zeros(size(f));
+    index(f>5)=f(f>5);
+    out_ind(y,:)=(I+lambda*laplacian)\transpose(index);
+    output(y,:)=output(y,:)./out_ind(y,:);
 end
 
 end
 
 end
+
+function [ output ] = fgs_wls_v( input, guidance, lambda, interpolate )
+%UNTITLED6 Summary of this function goes here
+%   Detailed explanation goes here
+
+output=zeros(size(input));
+out_ind=zeros(size(input));
+[rows,cols]=size(input);
+weights=zeros(rows,rows);
+laplacian=zeros(rows,rows);
+I=eye(rows);
+
+for x=1:cols
+
+    f=input(:,x);
+    g=guidance(:,x);
+    variance=var(g);
+
+    for p=1:rows
+
+        for q=1:rows
+
+            weights(p,q)=exp(-abs(g(p)-g(q))/variance);
+
+        end
+
+    end
+
+    for m=1:rows
+
+        for n=1:rows
+
+
+            if (m==n)
+                if (m==1)
+                    laplacian(m,n)=weights(m,m+1);
+                elseif (m==rows)
+                    laplacian(m,n)=weights(m,m-1);
+                else
+                    laplacian(m,n)=weights(m,m-1)+weights(m,m+1);
+                end
+            elseif (n==(m-1))||(n==(m+1))
+                laplacian(m,n)=-weights(m,n);
+            else
+                laplacian(m,n)=0;
+            end
+
+
+        end
+
+    end
+
+    output(:,x)=(I+lambda*laplacian)\f;
+
+    if (interpolate==1)
+        index=zeros(size(f));
+        index(f>5)=f(f>5);
+        out_ind(:,x)=(I+lambda*laplacian)\index;
+        output(:,x)=output(:,x)./out_ind(:,x);
+    end
+
+end
+
+end
+
 
