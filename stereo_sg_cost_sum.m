e1734048e400e6498650eb1812f16b34a74bbc37
DESKTOP-SCVHRLK\Denis <andreev.93@gmail.com>
2017-05-02 11:07:58 +0300

    changed the algorithm implementation

--- stereo_sg_cost_sum.m
+++ stereo_sg_cost_sum.m
@@ -2,6 +2,7 @@ function [ cost_s ] = stereo_sg_cost_sum( rows, cols, cost, max_disparity, P1, P
 %UNTITLED4 Summary of this function goes here
 %   Detailed explanation goes here
 
+cost(~isfinite(cost))=2*P2;
 cost_s=zeros(rows,cols,max_disparity);%инициализация нулями матрицы совокупных счетов
 
 %ограничения
@@ -11,54 +12,52 @@ wb=0;
 
 waitbar(wb/5)
 
-cost_s(1,:,:)=cost(1,:,:);
-cost_s(:,1,:)=cost(:,1,:);
-cost_s(rows,:,:)=cost(rows,:,:);
-cost_s(:,cols,:)=cost(:,cols,:);
+% cost_s(1,:,:)=cost(1,:,:);
+% cost_s(:,1,:)=cost(:,1,:);
+% cost_s(rows,:,:)=cost(rows,:,:);
+% cost_s(:,cols,:)=cost(:,cols,:);
 
 wb=0;
 waitbar(wb/4)
 
 x0=1;
-
 for y0=1:rows
 
     waitbar((wb+y0/rows)/4)
-    cost_s=stereo_sg_cost_travers( cost_s, x0, y0, cost, max_disparity, cols, rows, P1, P2 );
+    cost_s=cost_s+stereo_sg_cost_travers( x0, y0, cost, max_disparity, cols, rows, P1, P2 );
 
 end
 
 wb=1;
 waitbar(wb/4);
-x0=cols;
 
+x0=cols;
 for y0=1:rows
     
     waitbar((wb+y0/rows)/4)
-    cost_s=stereo_sg_cost_travers( cost_s, x0, y0, cost, max_disparity, cols, rows, P1, P2 );
+    cost_s=cost_s+stereo_sg_cost_travers( x0, y0, cost, max_disparity, cols, rows, P1, P2 );
 
 end
 
 wb=2;
 waitbar(wb/4);
-y0=1;
 
+y0=1;
 for x0=1:cols
     
-    waitbar((wb+x0/rows)/4)
-    cost_s=stereo_sg_cost_travers( cost_s, x0, y0, cost, max_disparity, cols, rows, P1, P2 );
+    waitbar((wb+x0/cols)/4)
+    cost_s=cost_s+stereo_sg_cost_travers( x0, y0, cost, max_disparity, cols, rows, P1, P2 );
 
 end
 
 wb=3;
 waitbar(wb/4);
+
 y0=rows;
-
 for x0=1:cols
-    
 
-    waitbar((wb+x0/rows)/4)
-    cost_s=stereo_sg_cost_travers( cost_s, x0, y0, cost, max_disparity, cols, rows, P1, P2 );
+    waitbar((wb+x0/cols)/4)
+    cost_s=cost_s+stereo_sg_cost_travers( x0, y0, cost, max_disparity, cols, rows, P1, P2 );
 
 end
 
--- stereo_sg_cost_travers.m
+++ stereo_sg_cost_travers.m
@@ -1,44 +1,50 @@
-function [ cost_s ] = stereo_sg_cost_travers( cost_s, x0, y0, cost, max_disparity, cols, rows, P1, P2 )
+function [ Lr ] = stereo_sg_cost_travers( x0, y0, cost, max_disparity, cols, rows, P1, P2 )
 %UNTITLED5 Summary of this function goes here
 %   Detailed explanation goes here
 
-minLi=zeros(1,max_disparity);
+%cost_s_new=zeros(size(cost_s));
+
+minLi=zeros(1,1,max_disparity);
+Lr=zeros(size(cost));
+Lr(y0,x0,:)=cost(y0,x0,:);
 
     for xr=-1:1
 
         for yr=-1:1
             
-            if ((xr~=0)||(yr~=0))
+%             if ~(((xr==-2)&&(yr==-2))||((xr==2)&&(yr==2))||((xr==0)&&(yr==0)))
             
                 x=x0+xr; y=y0+yr;
 
                 while (x>1)&&(x<cols)&&(y>1)&&(y<rows)
 
-                    minLk=min(cost(y-yr,x-xr,:));
+                    minLr=min(Lr(y-yr,x-xr,:));
 
                     for d=1:(max_disparity)
 
                         if (d==1)
-                            minLi(d)=min([minLk+P2 cost(y-yr,x-xr,d) cost(y-yr,x-xr,d+1)+P1]);
+                            minLi(1,1,d)=min([minLr+P2 Lr(y-yr,x-xr,d) Lr(y-yr,x-xr,d+1)+P1]);
                         elseif (d==(max_disparity))
-                            minLi(d)=min([minLk+P2 cost(y-yr,x-xr,d-1)+P1 cost(y-yr,x-xr,d)]);
+                            minLi(1,1,d)=min([minLr+P2 Lr(y-yr,x-xr,d-1)+P1 Lr(y-yr,x-xr,d)]);
                         else
-                            minLi(d)=min([minLk+P2 cost(y-yr,x-xr,d-1)+P1 cost(y-yr,x-xr,d) cost(y-yr,x-xr,d+1)+P1]);
+                            minLi(1,1,d)=min([minLr+P2 Lr(y-yr,x-xr,d-1)+P1 Lr(y-yr,x-xr,d) Lr(y-yr,x-xr,d+1)+P1]);
                         end
 
-                        cost_s(y,x,d)=cost_s(y,x,d)+cost(y,x,d)+minLi(d)-minLk;
-
                     end
+
+                    Lr(y,x,:)=cost(y,x,:)+minLi(1,1,:)-minLr;
 
                     x=x+xr; y=y+yr;
 
                 end
 
-            end
+%             end
             
         end
 
     end
+
+% cost_s=cost_s+Lr;
 
 end
 
