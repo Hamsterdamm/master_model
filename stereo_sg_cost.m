b5940713bf27d6340f598d71f67326e892120a98
DESKTOP-SCVHRLK\Denis <andreev.93@gmail.com>
2017-05-02 11:06:32 +0300

    inf costs by default

--- stereo_sg_cost.m
+++ stereo_sg_cost.m
@@ -5,7 +5,7 @@ function [ cost ] = stereo_sg_cost( left, right , init_disp, max_disparity)
 [rows,cols]=size(left);%размеры изображения
 
 P_I1_I2=zeros(256,256);%инициализация нулями взаимной плотности вероятности
-cost=zeros(rows,cols,max_disparity);%инициализация нулями матрицы счетов
+cost=inf(rows,cols,max_disparity);%инициализация нулями матрицы счетов
 
 %расчет взаимной информации
 
@@ -15,15 +15,16 @@ for x=1:cols
     for y=1:rows
 
         d=init_disp(y,x);%значение дальности для (x,y) пиксела
-        if isfinite(d)&&(x-d>0)&&(x-d<cols)%если смещенный пиксел находится в пределах изображения
-        i=left(y,x)+1;%i-я яркость - яркость пиксела (x,y)
-        k=right(y,x-d)+1;%k-я яркость - яркость смещенного пиксела (x-d,y)
-        %добавляем 1 т.к. яркости от 0 до 255, а массив плотности
-        %вероятности от 1 до 256
         
-        P_I1_I2(k,i)=P_I1_I2(k,i)+1;%запоминаем +1 пиксел 
-                                    %с яркостью i на левом изображении 
-                                    %и яркостью k на правом
+        if isfinite(d)&&(x-d>0)&&(x-d<cols)%если смещенный пиксел находится в пределах изображения
+            i=left(y,x)+1;%i-я яркость - яркость пиксела (x,y)
+            k=right(y,x-d)+1;%k-я яркость - яркость смещенного пиксела (x-d,y)
+            %добавляем 1 т.к. яркости от 0 до 255, а массив плотности
+            %вероятности от 1 до 256
+
+            P_I1_I2(k,i)=P_I1_I2(k,i)+1;%запоминаем +1 пиксел 
+                                        %с яркостью i на левом изображении 
+                                        %и яркостью k на правом
         end       
     end
 end
@@ -58,10 +59,9 @@ P_I2=sum(transpose(P_I1_I2));%суммируем все вероятности вдоль строк
 % h2=P_I2_F_log_F*(-1/num_correspondence);
 
 %энтропия
-h12=log(P_I1_I2)*(-1/num_correspondence);
-h1=log(P_I1)*(-1/num_correspondence);
-h2=log(P_I2)*(-1/num_correspondence);
-
+h12=-log(P_I1_I2)/num_correspondence;
+h1=-log(P_I1)/num_correspondence;
+h2=-log(P_I2)/num_correspondence;
 
 h = waitbar(0,'Cost computation in progress...');
 
@@ -78,7 +78,7 @@ for x=1:cols
             
                 i=left(y,x)+1;
                 k=right(y,x-d)+1;
-                cost(y,x,d+1)=-(h1(i)+h2(k)-h12(k,i));
+                cost(y,x,d+1)=-(-h12(k,i));%h1(i)+h2(k)
         
             end
             
@@ -88,9 +88,9 @@ for x=1:cols
     
 end
 
-%temp_cost=zeros(size(cost));
-cost(~isfinite(cost))=0;
-%cost=temp_cost;
+
+%cost(~isfinite(cost))=0;
+
 close(h);
 
 end
